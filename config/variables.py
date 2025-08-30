68f3876d4be85ed3b87657787bcfeba41f25f026
Riley de Man <rileydeman@gmail.com>
2025-08-30 02:19:29 +0200

    Main program ready
    Main program ready

---
+++ config/variables.py
@@ -0,0 +1,17 @@
+# Constants
+LINE = "\n=========================================\n"
+PROGRAM_COOLDOWN = 0.01
+KEYS = [
+    {
+        "name": "NoLookie Map Cover",
+        "input": "g",
+        "wasPressed": False,
+        "show": "ctrl+alt+f11",
+        "hide": "ctrl+alt+f12",
+        "delay": 0  # Require key to be held this miliseconds long before SHOW (0 = immediate)
+    },
+    # Add more keys if you want
+]
+
+# Variable
+runProgram = bool(True)
--- main.py
+++ main.py
@@ -6,4 +6,57 @@ checkPackages()
 # Imports
 import time
 import keyboard
+from config.variables import *
 
+# Functions
+def nowMS():
+    return time.time() * 1000
+
+def checkValidInput(inp, type):
+    for key in KEYS:
+        if key["input"] == inp:
+            isShown = bool(False)
+            wasPressed = key["wasPressed"]
+
+            if type == "KD":
+                key["wasPressed"] = True
+                execute = bool(False)
+                downSince = nowMS()
+
+                while not execute and not isShown:
+                    if (nowMS() - downSince) >= key["delay"]:
+                        execute = True
+
+                if execute:
+                    keyboard.send(key["show"])
+                    isShown = True
+
+            elif type == "KU":
+                if wasPressed:
+                    keyboard.send(key["hide"])
+
+
+print("Helper running. Press esc in this window to stop.")
+print("Listening for:\n")
+
+for key in KEYS:
+    print(f"{key["input"]}: {key['name']}")
+
+print(LINE)
+
+while runProgram:
+    e = keyboard.read_event()
+    key = e.name
+
+    if e.event_type == keyboard.KEY_DOWN:
+
+        print(f"Key down: {key}")
+
+        checkValidInput(key, "KD")
+
+        if key == "esc":
+            runProgram = False
+
+    if e.event_type == keyboard.KEY_UP:
+        print(f"Key released: {key}")
+        checkValidInput(key, "KU")
